c6d9f4dfc50ed940d82c858c9141946aa23fc31a
DeveloperNazmul <developernazmulbd@gmail.com>
2020-05-11 05:48:34 +0600

    Added remaining type definitions and bumped version

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,8 @@
 /// <reference types="node"/>
-// Type definitions for Jetty v0.2.1
+// Type definitions for Jetty v0.2.2
 // Project: https://github.com/fknsrs/jetty
-// Definitions by: Zihad <https://github.com/zihadmahiuddin>
+// Definitions by: Zihad <https://github.com/zihadmahiuddin>, Nazmul <https://github.com/DeveloperNazmul>
 // Definitions: https://github.com/zihadmahiuddin/jetty
-
 declare module "jetty" {
   export type StyleFunction = (str: string) => void;
 
@@ -17,12 +16,41 @@ declare module "jetty" {
     nuke: () => void;
     clear: () => void;
 
-    // codes array
     moveUp: (n: number | number[]) => void;
-    // TODO: add tyings for the rest of the methods
+    moveDown: (n: number | number[]) => void;
+    moveLeft: (n: number | number[]) => void;
+    moveRight: (n: number | number[]) => void;
+    lineUp: (n: number | number[]) => void;
+    lineDown: (n: number | number[]) => void;
+    save: (n: number | number[]) => void;
+    restore: (n: number | number[]) => void;
+    hide: (n: number | number[]) => void;
+    show: (n: number | number[]) => void;
+    sgr: (n: number | number[]) => void;
 
-    // sgrCodes array
     reset: (n: number) => void;
-    // TODO: add tyings for the rest of the methods
+    bold: (n: number) => void;
+    faint: (n: number) => void;
+    italic: (n: number) => void;
+    underline: (n: number) => void;
+    blink: (n: number) => void;
+    binkRapid: (n: number) => void;
+    imageNegative: (n: number) => void;
+    conceal: (n: number) => void;
+    strikeout: (n: number) => void;
+    boldOff: (n: number) => void;
+    normal: (n: number) => void;
+    italicOff: (n: number) => void;
+    underlineOff: (n: number) => void;
+    blinkOff: (n: number) => void;
+    imagePositive: (n: number) => void;
+    reveal: (n: number) => void;
+    strikeoutOff: (n: number) => void;
+    defaultColour: (n: number) => void;
+    defaultBackgroundColour: (n: number) => void;
+    frame: (n: number) => void;
+    encircle: (n: number) => void;
+    overline: (n: number) => void;
+    frameOff: (n: number) => void;
   }
 }
